a7a7d5d48bd18d682933e947c822400d8d217b66
ASKARALI001 <askartazhmuradov@gmail.com>
2022-09-09 00:45:33 +0600

    адаптатция 1стр закончена

--- src/js/main.js
+++ src/js/main.js
@@ -46,3 +46,14 @@ var meetSwiper = new Swiper(".meetSwiper", {
     },
 });
 
+
+let burger = document.querySelector('.burger')
+let headerList =document.querySelector('.header__list')
+// let headerBlock = document.querySelector('.header__block')
+
+burger.onclick = function () {
+    burger.classList.toggle('burger_active')
+    headerList.classList.toggle('header__list_active')
+    headerBlock.classList.toggle('header__block_active')
+}
+
